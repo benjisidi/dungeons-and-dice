53c4b522dd67ff2a4ef60e23fa8a2a293ad1d3c8
Benji Sidi <benjisidi@gmail.com>
2020-08-05 11:54:23 +0100

    Renamed variable d to f so we can use "ndf" notation Simplified the expression in expected_result

--- dice.py
+++ dice.py
@@ -3,24 +3,18 @@ import numpy as np
 import random
 
 
-class Dice:
-    @staticmethod
-    def roll_dice(n, d):
-        return [random.randint(1, d) for i in range(n)]
+def roll(n, f):
+    return [random.randint(1, f) for i in range(n)]
 
-    def __init__(self, dice_array: List[Tuple[int, int]]):
-        self.dice = dice_array
 
-    def roll(self):
-        return [self.roll_dice(n, d) for (n, d) in self.dice]
+def roll_cluster(dice_list, n=1):
+    return [[roll(n, f) for (n, f) in dice_list] for i in range(n)]
 
-    def roll_n(self, n):
-        return [self.roll() for i in range(n)]
 
-    def expected_result(self):
-        return [n * 1 / d * (1 + d) * d / 2 for (n, d) in self.dice]
+def expected_result(dice_list):
+    return [n * (f + 1) / 2 for (n, f) in dice_list]
 
 
 if __name__ == "__main__":
-    test = Dice([(1, 4), (2, 6)])
-    print(test.expected_result())
+    test_dice = [(1, 4), (2, 6)]
+    print(expected_result(test_dice))
